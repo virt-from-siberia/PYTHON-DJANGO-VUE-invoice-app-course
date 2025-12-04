3f9dbd74565bb500b8f344cb3827d9457d958b9e
Aleksey Elchin <alonso85@mail.ru>
2025-12-04 22:09:03 +0900

    Python Django and Vue Full Course - Build An Invoicing Web App 25 minutes

--- invoicely/invoicely_vue/src/App.vue
+++ invoicely/invoicely_vue/src/App.vue
@@ -1,30 +1,72 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrapper">
+    <nav class="navbar" is-dark>
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          <strong>Invoicely</strong>
+        </router-link>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <template v-if="$store.state.isAuthenticated">
+            <router-link to="/dashboard" class="navbar-item">
+              Dashboard
+            </router-link>
+          </template>
+
+          <template v-else>
+            <router-link to="/home" class="navbar-item">
+              Home
+            </router-link>
+
+            <div class="navbar-item">
+              <div class="buttons">
+                <router-link to="/sign-up" class="button is-success">
+                  <strong> Sign up</strong>
+                </router-link>
+                <router-link to="/log-in" class="button is-light">
+                  <strong>Log in</strong>
+                </router-link>
+              </div>
+            </div>
+          </template>
+
+        </div>
+      </div>
+    </nav>
+  </div>
+  <section class="section">
+    <router-view />
+  </section>
+  <footer class="footer">
+    <p class="has-text-centered">
+      Copyright (c) 2025
+    </p>
+  </footer>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
+<script>
+import axios from 'axios'
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+export default {
+  name: 'App',
+  beforeCreate() {
+    this.$store.commit('initializeStore')
 
-    &.router-link-exact-active {
-      color: #42b983;
+    const token = this.$store.state.token
+
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = "Token" + token
+    } else {
+      axios.defaults.headers.common['Authorization'] = ""
     }
   }
 }
+
+</script>
+
+<style lang="scss">
+@import "../node_modules/bulma";
 </style>
